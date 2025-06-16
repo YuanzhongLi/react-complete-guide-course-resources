aabeaccb19b4a31e444a3c66e860a2674fd08c1d
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2025-06-16 14:15:22 +0900

    finish

--- my-code/STYLING/vite.config.js
+++ my-code/STYLING/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// eslint-disable-next-line import/no-unresolved
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
