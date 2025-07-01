a44e553cd8b58708ef6e17884db94aeb4946cfd9
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2025-07-01 19:20:42 +0900

    finish

--- my-code/THE-ALMOST-FINAL-COUNTDOWN/src/App.jsx
+++ my-code/THE-ALMOST-FINAL-COUNTDOWN/src/App.jsx
@@ -1,10 +1,14 @@
 import Player from './components/Player';
+import TimerChallenge from './components/TimerChallenge';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges" />
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+      </div>
     </>
   );
 }
