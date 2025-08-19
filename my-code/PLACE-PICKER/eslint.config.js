d9edf3f0748a13624d2492160977f83ba45b3c0d
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2025-08-19 19:07:47 +0900

    init commit

---
+++ my-code/PLACE-PICKER/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+  { ignores: ["**/*.test.{js,mjs,cjs,jsx}", "node_modules/", "dist/", "build/", "*.min.js", "*.bundle.js"] },
+  { 
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    } 
+  },
+]);
